e8f328b7e183e04c00825356114d85d6c281d1b5
esa <esa.hekmat@gmail.com>
2017-10-23 16:34:00 +0330

    persist audits

--- mafiaTelegramBot/src/main/java/co/mafiagame/bot/handler/LangCommandHandler.java
+++ mafiaTelegramBot/src/main/java/co/mafiagame/bot/handler/LangCommandHandler.java
@@ -1,5 +1,6 @@
 package co.mafiagame.bot.handler;
 
+import co.mafiagame.bot.persistence.domain.Account;
 import co.mafiagame.bot.persistence.repository.AccountRepository;
 import co.mafiagame.bot.telegram.EditMessageReplyMarkupRequest;
 import co.mafiagame.bot.telegram.SendMessage;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
+import java.util.Objects;
 
 /**
  * @author Esa Hekmatizadeh
@@ -18,32 +20,35 @@ import java.util.Collections;
 @Component
 public class LangCommandHandler extends TelegramCallbackHandler {
 
-    @Autowired
-    private AccountRepository accountRepository;
+	@Autowired
+	private AccountRepository accountRepository;
 
-    @Override
-    protected String getCommandString() {
-        return Constants.Command.LANG;
-    }
+	@Override
+	protected String getCommandString() {
+		return Constants.Command.LANG;
+	}
 
 
-    @Override
-    public void execute(TCallBackQuery message) {
-        String lang = message.getData().substring(message.getData().indexOf(" ") + 1);
-        setLang(message.getFrom().getId(), MessageHolder.Lang.valueOf(lang));
-        client.editMessageReplyMarkup(new EditMessageReplyMarkupRequest()
-                .setChatId(message.getMessage().getChat().getId())
-                .setMessageId(message.getMessage().getId())
-                .setReplyMarkup(new TInlineKeyboardMarkup().setInlineKeyboard(Collections.emptyList()))
-        );
-        client.send(new SendMessage()
-                .setChatId(message.getMessage().getChat().getId())
-                .setText(MessageHolder.get("add.me.to.group", MessageHolder.Lang.valueOf(lang))));
-    }
+	@Override
+	public void execute(TCallBackQuery message) {
+		String lang = message.getData().substring(message.getData().indexOf(" ") + 1);
+		setLang(message.getFrom().getId(), MessageHolder.Lang.valueOf(lang), message);
+		client.editMessageReplyMarkup(new EditMessageReplyMarkupRequest()
+						.setChatId(message.getMessage().getChat().getId())
+						.setMessageId(message.getMessage().getId())
+						.setReplyMarkup(new TInlineKeyboardMarkup().setInlineKeyboard(Collections.emptyList()))
+		);
+		client.send(new SendMessage()
+						.setChatId(message.getMessage().getChat().getId())
+						.setText(MessageHolder.get("add.me.to.group", MessageHolder.Lang.valueOf(lang))));
+	}
 
-    private void setLang(Long userId, MessageHolder.Lang lang) {
-        accountRepository.save(
-                accountRepository.findByTelegramUserId(userId)
-                        .setLang(lang));
-    }
+	private void setLang(Long userId, MessageHolder.Lang lang, TCallBackQuery message) {
+		Account account = accountRepository.findByTelegramUserId(userId);
+		if (Objects.isNull(account)) {
+			account = accountRepository.save(new Account(message.getFrom()).setLang(lang));
+			accountCache.put(account.getTelegramUserId(), account);
+		} else
+			accountRepository.save(account.setLang(lang));
+	}
 }
--- mafiaTelegramBot/src/main/java/co/mafiagame/bot/handler/RegisterCommandHandler.java
+++ mafiaTelegramBot/src/main/java/co/mafiagame/bot/handler/RegisterCommandHandler.java
@@ -2,6 +2,9 @@ package co.mafiagame.bot.handler;
 
 import co.mafiagame.bot.Room;
 import co.mafiagame.bot.persistence.domain.Account;
+import co.mafiagame.bot.persistence.domain.Action;
+import co.mafiagame.bot.persistence.domain.Audit;
+import co.mafiagame.bot.persistence.repository.AuditRepository;
 import co.mafiagame.bot.telegram.SendMessage;
 import co.mafiagame.bot.telegram.SendMessageWithRemoveKeyboard;
 import co.mafiagame.bot.telegram.TMessage;
@@ -10,12 +13,10 @@ import co.mafiagame.bot.util.MessageHolder;
 import co.mafiagame.engine.Constants;
 import co.mafiagame.engine.GameMood;
 import co.mafiagame.engine.Player;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Objects;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -23,78 +24,95 @@ import java.util.stream.Collectors;
  */
 @Component
 public class RegisterCommandHandler extends TelegramCommandHandler {
+	@Autowired
+	private AuditRepository auditRepository;
 
-    @Override
-    protected Collection<String> getCommandString() {
-        return Arrays.asList(Constants.Command.REGISTER,
-            MessageHolder.get("register", MessageHolder.Lang.FA));
-    }
+	@Override
+	protected Collection<String> getCommandString() {
+		return Arrays.asList(Constants.Command.REGISTER,
+						MessageHolder.get("register", MessageHolder.Lang.FA));
+	}
 
-    @Override
-    public void execute(TMessage message) {
-        if (!message.isGroup()) {
-            MessageHolder.Lang lang = getLang(message);
-            sendMessage(message, "register.not.allowed.in.private", lang, false);
-            return;
-        }
-        Room room = gameContainer.room(message.getChat().getId());
-        if (Objects.isNull(room)) {
-            MessageHolder.Lang lang = getLang(message);
-            sendMessage(message, "register.before.start.error", lang, true);
-            return;
-        }
-        if (room.getGame().getGameMood() != GameMood.NOT_STARTED) {
-            sendMessage(message, "register.after.game.started.error", room.getLang(), true);
-            return;
-        }
-        Account account = accountCache.get(message.getFrom().getId());
-        if (Objects.isNull(account))
-            account = accountRepository.save(new Account(message.getFrom()).setLang(room.getLang()));
-        register(message, room, account);
+	@Override
+	public void execute(TMessage message) {
+		if (!message.isGroup()) {
+			MessageHolder.Lang lang = getLang(message);
+			sendMessage(message, "register.not.allowed.in.private", lang, false);
+			return;
+		}
+		Room room = gameContainer.room(message.getChat().getId());
+		if (Objects.isNull(room)) {
+			MessageHolder.Lang lang = getLang(message);
+			sendMessage(message, "register.before.start.error", lang, true);
+			return;
+		}
+		if (room.getGame().getGameMood() != GameMood.NOT_STARTED) {
+			sendMessage(message, "register.after.game.started.error", room.getLang(), true);
+			return;
+		}
+		Account account = accountCache.get(message.getFrom().getId());
+		if (Objects.isNull(account))
+			account = accountRepository.save(new Account(message.getFrom()).setLang(room.getLang()));
+		register(message, room, account);
 
-    }
+	}
 
-    private void register(TMessage message, Room room, Account account) {
-        boolean started = room.getGame().registerPlayer(String.valueOf(message.getFrom().getId()));
-        client.send(new SendMessageWithRemoveKeyboard()
-            .setReplyMarkup(new TReplyKeyboardRemove()
-                .setSelective(true))
-            .setReplyToMessageId(message.getId())
-            .setChatId(room.getRoomId())
-            .setText(MessageHolder.get("player.successfully.registered", room.getLang(),
-                accountCache.get(message.getFrom().getId()).fullName())));
-        gameContainer.putUserRoom(message.getFrom().getId(), message.getChat().getId());
-        room.getAccounts().add(account);
-        accountCache.put(account.getTelegramUserId(), account);
-        if (started) {
-            sendMessage(message, "game.started", room.getLang(), false);
-            room.getAccounts().forEach(a -> client.send(new SendMessage()
-                .setChatId(a.getTelegramUserId())
-                .setText(roleMsg(room, a))
-            ));
-        }
-    }
+	private void register(TMessage message, Room room, Account account) {
+		boolean started = room.getGame().registerPlayer(String.valueOf(message.getFrom().getId()));
+		client.send(new SendMessageWithRemoveKeyboard()
+						.setReplyMarkup(new TReplyKeyboardRemove()
+										.setSelective(true))
+						.setReplyToMessageId(message.getId())
+						.setChatId(room.getRoomId())
+						.setText(MessageHolder.get("player.successfully.registered", room.getLang(),
+										accountCache.get(message.getFrom().getId()).fullName())));
+		gameContainer.putUserRoom(message.getFrom().getId(), message.getChat().getId());
+		room.getAccounts().add(account);
+		accountCache.put(account.getTelegramUserId(), account);
+		if (started) {
+			sendMessage(message, "game.started", room.getLang(), false);
+			room.getAccounts().forEach(a -> {
+				auditRepository.save(new Audit()
+								.setAction(Action.START_GAME)
+								.setActor(a)
+								.setDate(new Date())
+								.setRoomId(String.valueOf(room.getRoomId()))
+				);
+				client.send(new SendMessage()
+								.setChatId(a.getTelegramUserId())
+								.setText(roleMsg(room, a))
+				);
+			});
 
-    public String roleMsg(Room room, Account a) {
-        switch (room.getGame().player(String.valueOf(a.getTelegramUserId())).getRole()) {
-            case CITIZEN:
-                return MessageHolder.get("your.role.is.citizen", room.getLang());
-            case DOCTOR:
-                return MessageHolder.get("your.role.is.doctor", room.getLang());
-            case DETECTIVE:
-                return MessageHolder.get("your.role.is.detective", room.getLang());
-            case MAFIA:
-                return MessageHolder.get("your.role.is.mafia", room.getLang()) + "\n" +
-                    MessageHolder.get("mafia.are.players", room.getLang(),
-                        room.getGame().mafias().stream()
-                            .map(Player::getUserId)
-                            .map(Long::valueOf)
-                            .map(room::findPlayer)
-                            .map(Optional::get)
-                            .map(Account::fullName)
-                            .collect(Collectors.joining(
-                                MessageHolder.get("and", room.getLang()))));
-        }
-        throw new IllegalStateException();
-    }
+		}
+	}
+
+	public String roleMsg(Room room, Account a) {
+		switch (room.getGame().player(String.valueOf(a.getTelegramUserId())).getRole()) {
+			case CITIZEN:
+				return MessageHolder.get("your.role.is.citizen", room.getLang());
+			case DOCTOR:
+				return MessageHolder.get("your.role.is.doctor", room.getLang());
+			case DETECTIVE:
+				return MessageHolder.get("your.role.is.detective", room.getLang());
+			case MAFIA:
+				auditRepository.save(new Audit()
+								.setAction(Action.BE_MAFIA)
+								.setActor(a)
+								.setDate(new Date())
+								.setRoomId(String.valueOf(room.getRoomId()))
+				);
+				return MessageHolder.get("your.role.is.mafia", room.getLang()) + "\n" +
+								MessageHolder.get("mafia.are.players", room.getLang(),
+												room.getGame().mafias().stream()
+																.map(Player::getUserId)
+																.map(Long::valueOf)
+																.map(room::findPlayer)
+																.map(Optional::get)
+																.map(Account::fullName)
+																.collect(Collectors.joining(
+																				MessageHolder.get("and", room.getLang()))));
+		}
+		throw new IllegalStateException();
+	}
 }
