e7765dee7d66bc9292347845b8b3b61244ed6ab3
esa <esa.hekmat@gmail.com>
2017-12-02 20:09:01 +0330

    what is my role when player is dead
    (cherry picked from commit da227b9)

--- mafiaTelegramBot/src/main/java/co/mafiagame/bot/handler/RegisterCommandHandler.java
+++ mafiaTelegramBot/src/main/java/co/mafiagame/bot/handler/RegisterCommandHandler.java
@@ -95,6 +95,6 @@ public class RegisterCommandHandler extends TelegramCommandHandler {
                             .collect(Collectors.joining(
                                 MessageHolder.get("and", room.getLang()))));
         }
-        throw new IllegalStateException();
+        return MessageHolder.get("your.dead",room.getLang());
     }
 }
